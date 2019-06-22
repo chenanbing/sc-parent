25296277dce43ff3aa15cffae9b6f73844133441
chenanbing <549006411@qq.com>
2019-06-22 15:35:26 +0800

    fgs

---
+++ sc-gateway/src/main/java/com/cab/scgateway/filter/UriKeyResolver.java
@@ -0,0 +1,19 @@
+package com.cab.scgateway.filter;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 根据uri限流
+ */
+//@Component
+public class UriKeyResolver  implements KeyResolver {
+
+    @Override
+    public Mono<String> resolve(ServerWebExchange exchange) {
+        return Mono.just(exchange.getRequest().getURI().getPath());
+    }
+
+}
