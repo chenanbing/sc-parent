d8b5c42009380729fb909011369e0e4cc1104492
chenanbing <549006411@qq.com>
2019-08-06 14:52:24 +0800

    而问题

---
+++ sc-lib/lib-redis/src/main/java/com/cab/lib/redis/RedissonUtil.java
@@ -0,0 +1,71 @@
+package com.cab.lib.redis;
+
+import com.cab.lib.redis.obj.ListObj;
+import com.cab.lib.redis.obj.MapObj;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisStringCommands;
+import org.springframework.data.redis.connection.ReturnType;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.types.Expiration;
+import org.springframework.util.CollectionUtils;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ */
+public class RedissonUtil {
+
+    private final Logger logger = LoggerFactory.getLogger(RedissonUtil.class);
+
+    private RedissonClient redissonClient;
+
+    public RedissonClient getRedissonClient() {
+        return redissonClient;
+    }
+
+    public void setRedissonClient(RedissonClient redissonClient) {
+        this.redissonClient = redissonClient;
+    }
+
+    public RLock lock(String lockKey) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.lock();
+        return lock;
+    }
+
+    private RLock lock(String lockKey, TimeUnit unit ,int timeout) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.lock(timeout, unit);
+        return lock;
+    }
+
+    public RLock lock(String lockKey, int leaseTime) {
+        return lock(lockKey,TimeUnit.SECONDS,leaseTime);
+    }
+
+    public boolean tryLock(String lockKey, TimeUnit unit, int waitTime, int leaseTime) {
+        RLock lock = redissonClient.getLock(lockKey);
+        try {
+            return lock.tryLock(waitTime, leaseTime, unit);
+        } catch (InterruptedException e) {
+            return false;
+        }
+    }
+
+    public void unlock(String lockKey) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.unlock();
+    }
+
+}
